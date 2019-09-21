705e9af03a46452cf46ffbfb9a35596470d3efda
Richard Palmer <richeytastic@gmail.com>
2019-09-21 14:46:07 +0800

    Reorganised header files.

--- cmake/LinkLibs.cmake
+++ cmake/LinkLibs.cmake
@@ -20,12 +20,9 @@ set_target_properties( ${PROJECT_NAME} PROPERTIES INSTALL_RPATH_USE_LINK_PATH TR
 install( TARGETS ${PROJECT_NAME} RUNTIME DESTINATION "bin"
                                  LIBRARY DESTINATION "lib"
                                  ARCHIVE DESTINATION "lib/static")
-install( FILES ${INCLUDE_FILES} DESTINATION "include")
-install( FILES "${PROJECT_BINARY_DIR}/${PROJECT_NAME}_Export.h" DESTINATION "include")
 
-if(EXISTS "${INCLUDE_DIR}/template")
-    install( DIRECTORY "${INCLUDE_DIR}/template" DESTINATION "include")
-endif()
+install( DIRECTORY "${INCLUDE_DIR}" DESTINATION "${CMAKE_INSTALL_PREFIX}")
+install( FILES "${PROJECT_BINARY_DIR}/${PROJECT_NAME}_Export.h" DESTINATION "include")
 
 install( FILES "${PROJECT_SOURCE_DIR}/cmake/${PROJECT_NAME}Config.cmake" DESTINATION "lib/cmake")
 install( FILES "${PROJECT_SOURCE_DIR}/cmake/Macros.cmake" DESTINATION "lib/cmake")
